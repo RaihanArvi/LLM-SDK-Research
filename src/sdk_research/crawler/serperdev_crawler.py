c604aeed7408643b2feb96105278f68b0b44267f
raihanarvi <rhnarvi@student.ubc.ca>
2025-10-23 09:37:08 -0700

    - finish serpdev_crawler.py

---
+++ src/sdk_research/crawler/serperdev_crawler.py
@@ -0,0 +1,71 @@
+from src.sdk_research.crawler.base_crawler import BaseCrawler
+import http.client
+import json
+
+"""
+Crawler using serper.dev.
+"""
+
+class SerperDevCrawler(BaseCrawler):
+
+    def __init__(self, serper_dev_api):
+        self.api = serper_dev_api
+
+        self.raw_results = ""
+        self.link_results = []
+        self.top_link_result = ""
+
+
+    def _crawl_links_raw(self, query):
+        """
+        Send request to serper.dev.
+        :return: multiline raw json response.
+        :except:
+        """
+
+        try:
+            conn = http.client.HTTPSConnection("google.serper.dev")
+            payload = json.dumps({
+                "q": f"{query}",
+            })
+            headers = {
+                'X-API-KEY': f'{self.api}',
+                'Content-Type': 'application/json'
+            }
+            conn.request("POST", "/search", payload, headers)
+            res = conn.getresponse()
+            data = res.read()
+            raw_json_str = data.decode("utf-8")
+
+            return json.loads(raw_json_str)
+
+        except Exception as e:
+            # Error placeholder
+            error_dict = {
+                "organic": [
+                    {
+                        "title": "First Organic Result: Python Lists",
+                        "link": "https://www.example.com/python-lists",
+                    }
+                ]
+            }
+
+            return error_dict
+
+
+    def _crawl_links(self, prompt) -> list[str]:
+        raw_response = self._crawl_links_raw(prompt)
+
+        links = []
+        for result in raw_response['organic']:
+            links.append(result['link'])
+
+        return links
+
+
+    def crawl(self, prompt):
+        self.link_results = self._crawl_links(prompt)
+        self.top_link_result = self.link_results[0]
+
+        return self.top_link_result
+
